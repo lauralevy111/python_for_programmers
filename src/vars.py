e839f977f4f5e6b4becbe659ca797397ce5b4b5a
lauralevy510 <llevy510@gmail.com>
2021-03-04 07:51:15 -0500

    practice w string methods: .upper(), .len(), .format(,,...)& .replace(,)

--- src/vars.py
+++ src/vars.py
@@ -6,7 +6,16 @@ pi = 3.14 #float
 #print(pi)
 
 firstName = "britney"
+firstName= firstName.upper()
+
 lastname= "spears"
+lastname= lastname.upper()
+
+namelen= len(lastname)
 
 
-print("my name is {} {}".format(firstName,lastname))
+
+
+sentence = ("my name is {} {}".format(firstName,lastname))
+
+print(sentence.replace("my", "My"))
