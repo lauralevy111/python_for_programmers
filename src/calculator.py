b1abedcaf29d40cbb78c0e5634d7a7590769e218
lauralevy510 <llevy510@gmail.com>
2021-03-04 07:20:28 -0500

    changed radius to 5.777, we're rounding area to 3 decimal places

--- src/calculator.py
+++ src/calculator.py
@@ -1,8 +1,10 @@
 pi = 3.14
-radius = 5
+radius = 5.777777777
 
 area = pi*radius*radius
 
-bigNumber = 5**100 #= five to the hundreth power
+area  = round(area,3)
 
-print(bigNumber)
+#bigNumber = 5**100 #= five to the hundreth power
+
+print(area)
