c7b6737b8669a88e8ea33c12cff603da705f182f
lauralevy510 <llevy510@gmail.com>
2021-03-05 08:03:06 -0500

    what happens hwen we test a valid string iin an if statment: it is true

--- src/if_elseif_else.py
+++ src/if_elseif_else.py
@@ -1,5 +1,5 @@
 
-age = 6
+age = 13
 
 even = (age % 2 == 0) # boolean: is the age even
 fiveToSeven = (7 >= age >= 5) #boolean: is the age between 5 & 7
@@ -9,3 +9,13 @@ if (even or fiveToSeven): #even is true OR fiveToSeven is true
 
 if (even and fiveToSeven): #even is true AND fiveToSeven is true
     print("and")
+
+#what happens when we test if a string is true
+n = "ajksfkadsjklf----002929"
+if n:   #it came out to true bc n != null
+    print("it worked")
+
+b
+
+if b:
+    print("it worked 2x")
