2d735aff9b6463585585cb1b203f245e2a239c51
lauralevy510 <llevy510@gmail.com>
2021-03-05 07:42:09 -0500

    we'recreatign 2 booleans, we're using 2 args in if statments, practicing use of 'or' and 'and'

--- src/if_elseif_else.py
+++ src/if_elseif_else.py
@@ -1,5 +1,11 @@
 
-age = 7
+age = 6
 
-if 7>= age >=5:
-    print("between five and seven")
+even = (age % 2 == 0) # boolean: is the age even
+fiveToSeven = (7 >= age >= 5) #boolean: is the age between 5 & 7
+
+if (even or fiveToSeven): #even is true OR fiveToSeven is true
+    print("or")
+
+if (even and fiveToSeven): #even is true AND fiveToSeven is true
+    print("and")
