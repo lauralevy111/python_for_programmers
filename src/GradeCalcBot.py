70186f3bfb8dd00dad3cdc8bf71b246bc8883a4d
lauralevy510 <llevy510@gmail.com>
2021-03-05 08:08:00 -0500

    added percent score to print

--- src/GradeCalcBot.py
+++ src/GradeCalcBot.py
@@ -26,4 +26,4 @@ elif(.69 >= percent >= .60):
 elif(.59 >= percent >= 0):
     letterGrade = "F"
 
-print("Student= {} \nLetterGrade = {}".format(studentName,letterGrade))
+print("Student = {} \nLetterGrade = {} \nPercentage score = {}".format(studentName,letterGrade,percent))
