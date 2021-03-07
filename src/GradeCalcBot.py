cf859d31bc5344e82b2d1b11f1140654536d39fb
lauralevy510 <llevy510@gmail.com>
2021-03-07 16:51:23 -0500

    commented gradecalc bot out entirely

--- src/GradeCalcBot.py
+++ src/GradeCalcBot.py
@@ -1,4 +1,4 @@
-
+'''
 pointsPossible = 100
 studentName = input("Enter student name: ")
 
@@ -10,7 +10,7 @@ except Exception:
 percent = round(score/pointsPossible,2)
 letterGrade = "ERROR"
 
-
+'''
 
 '''
 a= 100-90%
@@ -19,7 +19,7 @@ c= 79-70%
 d= 69-70%
 f= 59-0%
 '''
-
+'''
 if (1.00 >= percent >= .90):
     letterGrade = "A"
 elif(.89 >= percent >= .80):
@@ -28,8 +28,10 @@ elif(.79 >= percent >= .70):
     letterGrade = "C"
 elif(.69 >= percent >= .60):
     letterGrade = "D"
-elif(.59 >= percent >= .50)65y97053uy    letterGrade = "E"
+elif(.59 >= percent >= .50):
+    letterGrade = "E"
 elif(.59 >= percent >= .50):
     letterGrade = "F"
 
 print("Student = {} \nLetterGrade = {} \nPercentage score = {}".format(studentName,letterGrade,percent))
+'''
