1e007c62b1cf38d641cdbec688a2b0ca382704e7
lauralevy510 <llevy510@gmail.com>
2021-03-07 17:01:22 -0500

    put the grade calculating code in the calcPercentGrade func

--- src/GradeCalcBot.py
+++ src/GradeCalcBot.py
@@ -6,36 +6,40 @@ studentName = input("Enter student name: ")
 try:
     score =float(input("Enter test score: "))
     print("score = {}".format(score))
+
+
 except Exception:
     print("you need to enter a valid score")
 
 def calcPercentGrade():
     print("calculating grade")
-
-percent = round(score/pointsPossible,2)
-letterGrade = "ERROR"
+    percent = round(score/pointsPossible,2)
 
 
+    letterGrade = "ERROR"
 
-'''
-a= 100-90%
-b= 89-80%
-c= 79-70%
-d= 69-70%
-f= 59-0%
-'''
 
-if (1.00 >= percent >= .90):
-    letterGrade = "A"
-elif(.89 >= percent >= .80):
-    letterGrade = "B"
-elif(.79 >= percent >= .70):
-    letterGrade = "C"
-elif(.69 >= percent >= .60):
-    letterGrade = "D"
-elif(.59 >= percent >= .50):
-    letterGrade = "E"
-elif(.59 >= percent >= .50):
-    letterGrade = "F"
+
+    '''
+    a= 100-90%
+    b= 89-80%
+    c= 79-70%
+    d= 69-70%
+    f= 59-0%
+    '''
+
+    if (1.00 >= percent >= .90):
+        letterGrade = "A"
+    elif(.89 >= percent >= .80):
+        letterGrade = "B"
+    elif(.79 >= percent >= .70):
+        letterGrade = "C"
+    elif(.69 >= percent >= .60):
+        letterGrade = "D"
+    elif(.59 >= percent >= .50):
+        letterGrade = "E"
+    elif(.59 >= percent >= .50):
+        letterGrade = "F"
+
 
 print("Student = {} \nLetterGrade = {} \nPercentage score = {}".format(studentName,letterGrade,percent))
