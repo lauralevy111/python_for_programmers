1dc9387b205c6ab295b10f0eabc2a3dd09aad8b4
lauralevy510 <llevy510@gmail.com>
2021-03-09 10:22:36 -0500

    created a list and then created a tuple to demonstrate similarity

---
+++ src/tuples.py
@@ -0,0 +1,10 @@
+#Tuples! :)
+
+faveSingers = ["Dolly Parton", "Whitney Houston", "Mariah Carey", "Celine Dion","Lady Gaga"]
+
+print(type(faveSingers))
+
+
+#to make a tuple: you just make a list ^^^ but use parenthases instead of square brackets
+faveSingersTuple = ("Dolly Parton", "Whitney Houston", "Mariah Carey", "Celine Dion","Lady Gaga")
+print(type(faveSingersTuple))
