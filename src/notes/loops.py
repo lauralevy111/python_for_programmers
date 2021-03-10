641cc427951718a2e3b945331b71d7f52fa3f087
lauralevy510 <llevy510@gmail.com>
2021-03-10 14:36:39 -0500

    commented out the last 2 loops & cread a new one where we iterate through the items in faveSingers

--- src/notes/loops.py
+++ src/notes/loops.py
@@ -1,8 +1,14 @@
 #Loops
-
+'''
 for x in range(5):
     print(x)#printing iterator, x, starts at 0, ends @4
 
 print("******(***)")
 for n in range(5,12):
     print(n)#prints 5 - 11
+'''
+
+faveSingers = ["Dolly Parton", "Whitney Houston", "Mariah Carey", "Celine Dion","Lady Gaga","Whitney Houston"]
+
+for singer in faveSingers:
+    print(singer)
