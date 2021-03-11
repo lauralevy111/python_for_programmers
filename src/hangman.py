0b150d034330bf424b6936efc6da7bfad866437f
lauralevy510 <llevy510@gmail.com>
2021-03-11 15:01:16 -0500

    set up my initial prints that explain whats happening & asks for the firsts guess

--- src/hangman.py
+++ src/hangman.py
@@ -8,3 +8,10 @@ word = "orange"
 #if you guess a letter correct, it does NOT subtract a life
 #if you guess incorrect, decrement lives.
 #can the user guess all the letters before running out of lives
+
+print("WELCOME TO \n💀HANGMAN.PY💀"+
+"\n😱😟🤯😰🧟‍😱😟🤯😰🧟‍♂️😱😟🤯😰🧟‍♂️😱😟🤯😰🧟‍♂️😱😟🤯😰🧟‍♂️♂️"+
+" \nYOU MUST GUESS MY SECRET WORD🔮")
+wordList = list("orange")
+
+guess = input("please guess one letter of the secret word: ")
