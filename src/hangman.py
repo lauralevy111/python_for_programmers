5c7a92d08a945a8885280dfc34e404255c12519e
lauralevy510 <llevy510@gmail.com>
2021-03-11 15:19:58 -0500

    added a while loop & an if/elsif/else with a TON of todo items

--- src/hangman.py
+++ src/hangman.py
@@ -11,7 +11,19 @@ word = "orange"
 
 print("WELCOME TO \n💀HANGMAN.PY💀"+
 "\n😱😟🤯😰🧟‍😱😟🤯😰🧟‍♂️😱😟🤯😰🧟‍♂️😱😟🤯😰🧟‍♂️😱😟🤯😰🧟‍♂️♂️"+
+"\n_ _ _ _ _ _ "+
 " \nYOU MUST GUESS MY SECRET WORD🔮")
-wordList = list("orange")
+wordList = list(word)
 
-guess = input("please guess one letter of the secret word: ")
+
+while len(wordList) != 0:
+    guess = input("Please guess a leter from my secret word: ")
+    if guess =="correct": #TODO: the letter guessed DOES exist in the secret wordList
+        #TODO: print the word with blanks on letters unguessed.
+        #TODO: get us back to line 20 for another guess
+    elsif guess=="allreadyguessedletter": #todo: the letter was already unguessed
+        #TODO: get us back to line 20 for anoither Guess
+    else : #TODO: guess is incorrect
+        #TODO: decrement lives
+        #TODO: print lives left
+        #TODO: if lives<=0: break, you are out of lives
