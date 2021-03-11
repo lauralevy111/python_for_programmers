8515d1eeb4455f94697fffd589b71237c5a978e2
lauralevy510 <llevy510@gmail.com>
2021-03-11 14:53:10 -0500

    added title, given code, & project brief

---
+++ src/hangman.py
@@ -0,0 +1,10 @@
+#Hangman
+
+#given code
+lives = 10 # = # of incorrect guesses
+word = "orange"
+
+#project brief
+#if you guess a letter correct, it does NOT subtract a life
+#if you guess incorrect, decrement lives.
+#can the user guess all the letters before running out of lives
