88fd65cabcb065fffcccd75646817c9c7387be21
lauralevy510 <llevy510@gmail.com>
2021-03-12 12:42:36 -0500

    added a definition of a dictionary

---
+++ src/notes/dictionaries.py
@@ -0,0 +1,5 @@
+#Dictionaries = stores key/value pairs
+'''each value in the list has a number associated with it
+in a dictionary  you have these things called keys
+you want a value, you give the key to get the value.
+'''
