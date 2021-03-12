0e8e887c7b754f2853cdd33d479f696de8d6c47e
lauralevy510 <llevy510@gmail.com>
2021-03-12 12:51:07 -0500

    added 4 dogs to dictionary

--- src/notes/dictionaries.py
+++ src/notes/dictionaries.py
@@ -4,4 +4,4 @@ in a dictionary  you have these things called keys
 you want a value, you give the key to get the value.
 '''
 
-dogs= {}#name = key, age = value
+dogs= {"Zilpha":8, "Beauregard":1,"Gidget":1, "Apollo":0}#name = key, age = value
