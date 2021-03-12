5c09b2b0ac11be3bf1421fcabdf76076e38cd600
lauralevy510 <llevy510@gmail.com>
2021-03-12 12:53:01 -0500

    refactor: renamed dogs dictionary to more appropriate name dogsAges

--- src/notes/dictionaries.py
+++ src/notes/dictionaries.py
@@ -4,6 +4,6 @@ in a dictionary  you have these things called keys
 you want a value, you give the key to get the value.
 '''
 
-dogs= {"Zilpha":8, "Beauregard":1,"Gidget":1, "Apollo":0}#name = key, age = value
+dogsAges= {"Zilpha":8, "Beauregard":1,"Gidget":1, "Apollo":0}#name = key, age = value
 
 print(dogs)
