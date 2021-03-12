7fb57d0713137c5848a2f30a879632cfc76631b6
lauralevy510 <llevy510@gmail.com>
2021-03-12 13:07:31 -0500

    now we're deleting from dictionaries

--- src/notes/dictionaries.py
+++ src/notes/dictionaries.py
@@ -4,13 +4,17 @@ in a dictionary  you have these things called keys
 you want a value, you give the key to get the value.
 '''
 
-dogsAges= {"Zilpha":8, "Beauregard":1,"Gidget":1, "Apollo":0}#name = key, age = value
+dogsAges= {"Zilpha":8, "Greyson":5, "Beauregard":1,"Gidget":1, "Apollo":0}#name = key, age = value
 '''keys have to be mutable, ints floats strings all work. tuple is immutable so that wont work.
 you can add a item to this dictionary with an int key and a string value! and that'll be okay!
+
+dictionaries are organized by key ONLY!! they have no order
 '''
 
 #adding to Dictionaries
 dogsAges["Mason"]=7
 
+#deleting :
+del(dogsAges["Greyson"])
 
 print(dogsAges)
