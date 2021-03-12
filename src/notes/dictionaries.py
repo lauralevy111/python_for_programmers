987db335d39e9d1fe4e768d5c3197fa5f5bf99f2
lauralevy510 <llevy510@gmail.com>
2021-03-12 13:02:53 -0500

    we're adding a value to dogsAges for mason

--- src/notes/dictionaries.py
+++ src/notes/dictionaries.py
@@ -5,9 +5,12 @@ you want a value, you give the key to get the value.
 '''
 
 dogsAges= {"Zilpha":8, "Beauregard":1,"Gidget":1, "Apollo":0}#name = key, age = value
-
 '''keys have to be mutable, ints floats strings all work. tuple is immutable so that wont work.
 you can add a item to this dictionary with an int key and a string value! and that'll be okay!
 '''
+
+#adding to Dictionaries
+dogsAges["Mason"]=7
+
 
 print(dogsAges)
