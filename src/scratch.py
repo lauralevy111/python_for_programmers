e195fd9c5936022b9e9d25cd93398a6504131619
lauralevy510 <llevy510@gmail.com>
2021-03-15 17:17:19 -0400

    added some more note sto createEmployee function (we're plotting the structure of this thing

--- src/scratch.py
+++ src/scratch.py
@@ -3,7 +3,11 @@
 #TODO: create vars required to onboard team member
 
 #TODO: create functions required to create team member feedback encapsulated
-#TODO: def createEmployee(name, department, hiringManagerID):
+#TODO: def createEmployee(name, department, hiringManagerID, salaryRate):
+    #this function will :
+        #create a unique employeeID
+        #add employee to department
+        #add employee to budget list
 #TODO: def assignToTeam(employeeID, teamID):
     #have user specify team to add employee to
 #TODO: def teamMemberTransfer(employee, destination):
