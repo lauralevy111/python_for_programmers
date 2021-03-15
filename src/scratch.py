545626eda4a16f88f797b9237489ec13b9e292b7
lauralevy510 <llevy510@gmail.com>
2021-03-15 17:08:08 -0400

    i'm figuring out what the methods are going ot looklike

--- src/scratch.py
+++ src/scratch.py
@@ -3,6 +3,7 @@
 #TODO: create vars required to onboard team member
 
 #TODO: create functions required to create team member feedback encapsulated
-#TODO: enter employee
-#TODO: have user specify team to add employee to
+#TODO: def createEmployee():
+#TODO: def assignToTeam():
+    #have user specify team to add employee to
 #TODO: def addToTeam
