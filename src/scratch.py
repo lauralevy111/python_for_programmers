0089a16ea06ed5825684e8bc59ab15889a94daf1
lauralevy510 <llevy510@gmail.com>
2021-03-15 17:05:28 -0400

    added some todo items (we're planning this

---
+++ src/scratch.py
@@ -0,0 +1,8 @@
+#scrach
+
+#todo: create vars required to onboard team member
+
+#todo: create functions required to create team member feedback encapsulated
+#enter employee
+#have user specify team to add employee to
+#def addToTeam
