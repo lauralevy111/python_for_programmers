859901b56719495f6f3bd94cd3913e8553926a6b
lauralevy510 <llevy510@gmail.com>
2021-03-15 17:06:44 -0400

    regulated my todo notation

--- src/scratch.py
+++ src/scratch.py
@@ -1,8 +1,8 @@
 #scrach
 
-#todo: create vars required to onboard team member
+#TODO: create vars required to onboard team member
 
-#todo: create functions required to create team member feedback encapsulated
-#enter employee
-#have user specify team to add employee to
-#def addToTeam
+#TODO: create functions required to create team member feedback encapsulated
+#TODO: enter employee
+#TODO: have user specify team to add employee to
+#TODO: def addToTeam
