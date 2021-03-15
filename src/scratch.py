24c369629a78bdd194822ef733f11f32a94759d7
lauralevy510 <llevy510@gmail.com>
2021-03-15 17:12:08 -0400

    added name, department, and hiringManagerID as parameters into create employee

--- src/scratch.py
+++ src/scratch.py
@@ -3,7 +3,7 @@
 #TODO: create vars required to onboard team member
 
 #TODO: create functions required to create team member feedback encapsulated
-#TODO: def createEmployee():
+#TODO: def createEmployee(name, department, hiringManagerID):
 #TODO: def assignToTeam():
     #have user specify team to add employee to
 #TODO: def teamMemberTransfer(employee, destination):
