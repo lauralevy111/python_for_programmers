5085c4b165c268bc9cc80774ba6727fa53d461dc
lauralevy510 <llevy510@gmail.com>
2021-04-07 07:34:49 -0400

    added a hello to the dog class

--- src/notes/classes.py
+++ src/notes/classes.py
@@ -1,7 +1,9 @@
 #like a blueprint for the py app
 #so you can create as many objs liek this class as ou like
+#instance variable = usuallly unique
 
 class Dog:
+    "Hello new dog"
 
 dog1 = Dog()
 dog2 = Dog()
