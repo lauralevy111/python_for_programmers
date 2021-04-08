20cf4553afdd15e75bdd7bbb1139b1845fa143ad
lauralevy510 <llevy510@gmail.com>
2021-04-08 09:41:42 -0400

    added instance of dog

--- src/notes/init.py
+++ src/notes/init.py
@@ -7,3 +7,5 @@ class Dog:
         #a function inside a class like this, is called a method, theyre diff but thats the diff
         #this method will get called every time u create an instance of this class :
         print("inside __init__(self) hello, Laura !")
+
+dog1 = Dog()
