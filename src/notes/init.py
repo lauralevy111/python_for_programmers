7b6fe7d840e29dfa5d67381a5c33732f9689dfad
lauralevy510 <llevy510@gmail.com>
2021-04-08 09:37:56 -0400

    added note

--- src/notes/init.py
+++ src/notes/init.py
@@ -2,4 +2,6 @@
 
 class Dog:
 
-    def __init__(self): #this is an init method, you always need to call it like this with the  2 underscores
+    def __init__(self):
+        #^^this is an init method, you always need to call it like this with the  2 underscores
+        #a function inside a class like this, is called a method, theyre diff but thats the diff
