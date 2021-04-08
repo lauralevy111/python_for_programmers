ce4bd351e4c256ef58b6f2faab817316164f0b0b
lauralevy510 <llevy510@gmail.com>
2021-04-08 09:50:52 -0400

    changed dog class to cat class

--- src/notes/init.py
+++ src/notes/init.py
@@ -1,13 +1,13 @@
 #Init
 
-class Dog:
+class Cat:
 
     def __init__(self):     #wahts self? in python when u create a new obj it will call the init meothod if u have one.
                                 # you can use anyword you want in place of self, its traditional to do self.
-s                                            
+
         #^^this is an init method, you always need to call it like this with the  2 underscores
         #a function inside a class like this, is called a method, theyre diff but thats the diff
         #this method will get called every time u create an instance of this class :
         print("inside __init__(self) hello, Laura !")
 
-dog1 = Dog()
+cat1 = Cat()
