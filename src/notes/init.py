5a12c604f75741e7402e0d68c0e557ae24da74b0
lauralevy510 <llevy510@gmail.com>
2021-04-08 09:41:16 -0400

    added print in init method

--- src/notes/init.py
+++ src/notes/init.py
@@ -5,3 +5,5 @@ class Dog:
     def __init__(self):
         #^^this is an init method, you always need to call it like this with the  2 underscores
         #a function inside a class like this, is called a method, theyre diff but thats the diff
+        #this method will get called every time u create an instance of this class :
+        print("inside __init__(self) hello, Laura !")
