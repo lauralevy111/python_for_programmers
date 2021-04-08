1f6cf59fdf112ba6fd5813892cc6940e4d31801b
lauralevy510 <llevy510@gmail.com>
2021-04-08 09:43:26 -0400

    added note on self

--- src/notes/init.py
+++ src/notes/init.py
@@ -2,7 +2,9 @@
 
 class Dog:
 
-    def __init__(self):
+    def __init__(self):     #wahts self? in python when u create a new obj it will call the init meothod if u have one.
+                                # you can use anyword you want in place of self, its traditional to do self.
+s                                            
         #^^this is an init method, you always need to call it like this with the  2 underscores
         #a function inside a class like this, is called a method, theyre diff but thats the diff
         #this method will get called every time u create an instance of this class :
