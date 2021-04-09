66c9acdfc5fa4590875b339b1b4c776aa7500f8f
lauralevy510 <llevy510@gmail.com>
2021-04-09 13:03:04 -0400

    removed bark_goodbye call & added self to parameters for  bark_hello

--- src/notes/init.py
+++ src/notes/init.py
@@ -7,8 +7,9 @@ class Dog:
         self.fur_color = fur_color
         self.weight = weight
         print("creating a dog")
+        #print(self.weight)
 
-    def bark_hello():
+    def bark_hello(self):
         print("Hello, bark bark!")
 
     def bark_goodbye():
@@ -16,4 +17,3 @@ class Dog:
 
 dog1 = Dog("Zilpha",8,"White",10)
 dog1.bark_hello()
-dog1.bark_goodbye()
