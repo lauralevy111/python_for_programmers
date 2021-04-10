96123b5e7390647325528a34c24baf42b7372af2
lauralevy510 <llevy510@gmail.com>
2021-04-10 09:00:21 -0400

    added Car class & init method with self + 4 feilds

--- src/notes/inheritance.py
+++ src/notes/inheritance.py
@@ -1,1 +1,8 @@
 #inheritance
+
+class Car:
+    def __init__(self,year, make, model, miles):
+        self.year = year
+        self.make = make
+        self.model = model
+        self.miles = miles
